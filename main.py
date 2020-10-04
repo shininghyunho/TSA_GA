664cc8327175c04894ef8b19dd157101c15c4ae6
hyunho <gusgh3315@gmail.com>
2020-10-04 19:01:52 +0900

    first

---
+++ main.py
@@ -0,0 +1,299 @@
+import sys
+import math, random
+import csv
+
+class TourManager:
+    destinationCities = []
+
+    def addCity(self, city):
+        self.destinationCities.append(city)
+
+    def getCity(self, index):
+        return self.destinationCities[index]
+
+    def numberOfCities(self):
+        return len(self.destinationCities)
+
+class City:
+    def __init__(self, x=None, y=None):
+        self.x = None
+        self.y = None
+        if x is not None:
+            self.x = x
+        else:
+            self.x = int(random.random() * 200)
+        if y is not None:
+            self.y = y
+        else:
+            self.y = int(random.random() * 200)
+
+    def getX(self):
+        return self.x
+
+    def getY(self):
+        return self.y
+
+    def distanceTo(self, city):
+        xDistance = abs(self.getX() - city.getX())
+        yDistance = abs(self.getY() - city.getY())
+        distance = math.sqrt((xDistance * xDistance) + (yDistance * yDistance))
+        return distance
+
+    def __repr__(self):
+        return str(self.getX()) + "," + str(self.getY())
+
+class Tour:
+    def __init__(self, tourmanager, tour=None):
+        self.tourmanager = tourmanager
+        self.tour = []
+        self.fitness = 0.0
+        self.distance = 0
+        if tour is not None:
+            self.tour = tour
+        else:
+            for i in range(0, self.tourmanager.numberOfCities()):
+                self.tour.append(None)
+
+    def __len__(self):
+        return len(self.tour)
+
+    def __getitem__(self, index):
+        return self.tour[index]
+
+    def __setitem__(self, key, value):
+        self.tour[key] = value
+
+    def __repr__(self):
+        geneString = 'Start -> '
+        for i in range(0, self.tourSize()):
+            geneString += str(self.getCity(i)) + ' -> '
+        geneString += 'End'
+        return geneString
+
+    def generateIndividual(self):
+        for cityIndex in range(0, self.tourmanager.numberOfCities()):
+            self.setCity(cityIndex, self.tourmanager.getCity(cityIndex))
+        random.shuffle(self.tour)
+
+    def getCity(self, tourPosition):
+        return self.tour[tourPosition]
+
+    def setCity(self, tourPosition, city):
+        self.tour[tourPosition] = city
+        self.fitness = 0.0
+        self.distance = 0
+
+    def getFitness(self):
+        if self.fitness == 0:
+            self.fitness = 1/float(self.getDistance())
+        return self.fitness
+
+    def getDistance(self):
+        if self.distance == 0:
+            tourDistance = 0
+            for cityIndex in range(0, self.tourSize()):
+                fromCity = self.getCity(cityIndex)
+                destinationCity = None
+                if cityIndex+1 < self.tourSize():
+                    destinationCity = self.getCity(cityIndex+1)
+                else:
+                    destinationCity = self.getCity(0)
+                tourDistance += fromCity.distanceTo(destinationCity)
+            # connect broken connection
+            fromCity = self.getCity(self.tourSize()-1)
+            destinationCity = self.getCity(0)
+            tourDistance += fromCity.distanceTo(destinationCity)
+
+            self.distance = tourDistance
+        return self.distance
+
+    def tourSize(self):
+        return len(self.tour)
+
+    def containsCity(self, city):
+        return city in self.tour
+
+
+class Population:
+    def __init__(self, tourmanager, populationSize, initialise):
+        self.tours = []
+        for i in range(0, populationSize):
+            self.tours.append(None)
+
+        if initialise:
+            for i in range(0, populationSize):
+                newTour = Tour(tourmanager)
+                newTour.generateIndividual()
+                self.saveTour(i, newTour)
+
+    def __setitem__(self, key, value):
+        self.tours[key] = value
+
+    def __getitem__(self, index):
+        return self.tours[index]
+
+    def saveTour(self, index, tour):
+        self.tours[index] = tour
+
+    def getTour(self, index):
+        return self.tours[index]
+
+    def getFittest(self):
+        fittest = self.tours[0]
+        for i in range(0, self.populationSize()):
+            if fittest.getFitness() <= self.getTour(i).getFitness():
+                fittest = self.getTour(i)
+        return fittest
+
+    def populationSize(self):
+        return len(self.tours)
+
+
+class GA:
+    def __init__(self, tourmanager, mutationRate=0.05, tournamentSize=5, elitism=True):
+        self.tourmanager = tourmanager
+        self.mutationRate = mutationRate
+        self.tournamentSize = tournamentSize
+        self.elitism = elitism
+
+    def evolvePopulation(self, pop):
+        newPopulation = Population(self.tourmanager, pop.populationSize(), False)
+        elitismOffset = 0
+        if self.elitism:
+            newPopulation.saveTour(0, pop.getFittest())
+            elitismOffset = 1
+
+        for i in range(elitismOffset, newPopulation.populationSize()):
+            parent1 = self.tournamentSelection(pop)
+            parent2 = self.tournamentSelection(pop)
+            child = self.crossover(parent1, parent2)
+            newPopulation.saveTour(i, child)
+
+        for i in range(elitismOffset, newPopulation.populationSize()):
+            self.mutate(newPopulation.getTour(i))
+
+        return newPopulation
+
+    def crossover(self, parent1, parent2):
+        child = Tour(self.tourmanager)
+
+        startPos = int(random.random() * parent1.tourSize())
+        endPos = int(random.random() * parent1.tourSize())
+
+        for i in range(0, child.tourSize()):
+            if startPos < endPos and i > startPos and i < endPos:
+                child.setCity(i, parent1.getCity(i))
+            elif startPos > endPos:
+                if not (i < startPos and i > endPos):
+                    child.setCity(i, parent1.getCity(i))
+
+        for i in range(0, parent2.tourSize()):
+            if not child.containsCity(parent2.getCity(i)):
+                for ii in range(0, child.tourSize()):
+                    if child.getCity(ii) == None:
+                        child.setCity(ii, parent2.getCity(i))
+                        break
+
+        return child
+
+    def mutate(self, tour):
+        for tourPos1 in range(0, tour.tourSize()):
+            if random.random() < self.mutationRate:
+                tourPos2 = int(tour.tourSize() * random.random())
+
+                city1 = tour.getCity(tourPos1)
+                city2 = tour.getCity(tourPos2)
+
+                tour.setCity(tourPos2, city1)
+                tour.setCity(tourPos1, city2)
+
+    def tournamentSelection(self, pop):
+        tournament = Population(self.tourmanager, self.tournamentSize, False)
+        for i in range(0, self.tournamentSize):
+            randomId = int(random.random() * pop.populationSize())
+            tournament.saveTour(i, pop.getTour(randomId))
+        fittest = tournament.getFittest()
+        return fittest
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    TSP_FILE = sys.argv[1]
+    f = open(TSP_FILE, 'r')
+    lines = f.readlines()
+
+    n_cities = 0
+    if(len(sys.argv)>=4):
+        if(sys.argv[2]=='-p'):
+            population_size=int(sys.argv[3])
+    else:
+        population_size = 100
+    n_generations = 20
+    random.seed(2020)
+    cities_num = {}
+    idx = 0
+
+    # Setup cities and tour
+    tourmanager = TourManager()
+
+    isNode = False
+    for line in lines:
+        if "DIMENSION" in line:
+            line = line.split()
+            n_cities = int(line[1])
+        elif "NODE_COORD_SECTION" in line | "DISPLAY_DATA_SECTION" in line:
+            isNode = True
+        elif "EOF" in line:
+            break
+        elif isNode == True & ("EOF" not in line):
+            line = line.split()
+            x = float(line[1])
+            idx = idx + 1
+            if x not in cities_num:
+                cities_num[x] = {}
+            y = float(line[2])
+            cities_num[x][y] = idx
+            tourmanager.addCity(City(x=x, y=y))
+
+    # Initialize population
+    pop = Population(tourmanager, populationSize=population_size, initialise=True)
+
+    # Evolve population
+    ga = GA(tourmanager)
+
+    for i in range(n_generations):
+        pop = ga.evolvePopulation(pop)
+
+    fittest = pop.getFittest()
+    print(fittest.getDistance())
+
+    # Save route
+    result = []
+    # Cities Index
+    for i in range(n_cities):
+        pos_x = fittest.getCity(i).getX()
+        pos_y = fittest.getCity(i).getY()
+        result.append(cities_num[pos_x][pos_y])
+
+    # Change 1 to -1
+    before_1=[]
+    after_1=[]
+    isOne=False
+    for city_num in result:
+        if isOne==False:
+            if city_num==1:
+                before_1.append(-1)
+            else:
+                before_1.append(city_num)
+        if city_num==1:
+            isOne=True
+        if isOne:
+            after_1.append(city_num)
+    result=[]
+    result=after_1+before_1
+
+    # Save Result
+    with open('solution.csv', 'w') as f:
+        writer = csv.writer(f,lineterminator='\n')
+        for route in result:
+            li=[route]
+            writer.writerow(li)
